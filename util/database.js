616359e07d7fb07cc8731a8345356c25c98dcfe4
Vinayjais <17vinayjaiswal>
2023-09-25 17:51:33 +0530

    db

--- util/database.js
+++ util/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('v-chat','root','241021@Vinay',{
+const sequelize = new Sequelize('v-chat','vinay','vinay2410',{
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'database-1.c4ydiiv1lqn6.eu-north-1.rds.amazonaws.com'
 });
 
 module.exports = sequelize;
+
+//241021@Vinay
